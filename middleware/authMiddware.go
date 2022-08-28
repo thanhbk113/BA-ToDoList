55353d37e49872d8a1c25849d83141410d17e2e9
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-08-28 20:24:05 +0700

    Access-Control-Allow-Headers

--- middleware/authMiddware.go
+++ middleware/authMiddware.go
@@ -42,7 +42,8 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, x-user-id, x-user-role, origin, Cache-Control, X-Requested-With")
+		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, x-user-id, x-user-role, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
 
 		if c.Request.Method == "OPTIONS" {
